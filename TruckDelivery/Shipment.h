01fa557428c54089cb515c04e2e1a9ee1c35cfeb
Jaaved Dewan <jilred15@gmail.com>
2023-03-15 18:55:58 -0400

    Update Shipment.h
    Added one extra global variable.
    Added some comments.

--- TruckDelivery/Shipment.h
+++ TruckDelivery/Shipment.h
@@ -4,13 +4,14 @@
 
 #define MIN_WEIGHT = 1;
 #define MAX_WEIGHT = 1000;
+#define MIN_SIZE = 0.25;
 #define MAX_SIZE = 1.0;
 
 struct Shipment
 {
-	int m_weight;
-	double m_size;
-	char m_destination[3];
+	int m_weight; //Between 1 to 1000 kg
+	double m_size; //Between 0.25 to 1 cubic metre
+	char m_destination[3]; //Max 3 characters
 };
 
 #endif
