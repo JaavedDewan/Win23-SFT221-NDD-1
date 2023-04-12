3c2b73ff5933cbdcbf5da4d2d832d9f0f7d42c95
Jaaved Dewan <jdewan@myseneca.ca>
2023-04-12 16:21:29 -0400

    UnitTest & .git/hook commit test 2

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
